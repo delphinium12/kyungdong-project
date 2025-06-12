57ee8a7affcb5a4a4a3a129bede56b8a32db7d9f
delphinium12 <ichie8699@gmail.com>
2025-06-12 21:06:39 +0900

    0612

--- js/common.js
+++ js/common.js
@@ -1,8 +1,11 @@
 const get = (target) => document.querySelector(target);
 const getAll = (target) => document.querySelectorAll(target);
 
+// #누르면 새 창 뜨는 - 링크막아주기
 const preventDefaultAnchor = () => {
     const $links = getAll('a[href="#"]');
+    $links.forEach((link) => link.addEventListener('click', (e) => e.preventDefault()));
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 };
 
 const navi = () => {
@@ -10,7 +13,55 @@ const navi = () => {
 };
 const familySite = () => {};
 
-const comInit = () => {};
+const siteMap = () => {
+    const $sitemap = get('.sitemap');
+    const $bg = get('.bg');
+    const $allmenu = get('#header .util .all-menu');
+    const $close = get('.sitemap .close');
+
+    $allmenu.addEventListener('click', (e) => {
+        $sitemap.classList.add('on');
+        $bg.classList.add('on');
+    });
+    $close.addEventListener('click', (e) => {
+        $sitemap.classList.remove('on');
+        $bg.classList.remove('on');
+    });
+};
+
+// 스크롤처리
+const topBar = () => {
+    const $top = get('.top');
+    const $header = get('#header');
+
+    let ty = 0;
+    $top.addEventListener('click', (e) => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+    window.addEventListener('scroll', (e) => {
+        //window.scrollY , window.pageYOffset
+        // console.log(window.scrollY);
+        ty = window.scrollY;
+        if (ty > 300) {
+            $top.classList.add('on');
+            //top에 on 붙이기
+        } else {
+            $top.classList.remove('on');
+        }
+        if (ty > 100) {
+            $header.classList.add('on');
+            //top에 on 붙이기
+        } else {
+            $header.classList.remove('on');
+        }
+    });
+};
+const comInit = () => {
+    navi();
+    topBar();
+    siteMap();
+};
 
 (() => {
     preventDefaultAnchor();
--- js/main.js
+++ js/main.js
@@ -3,10 +3,124 @@ const mainBanner = () => {};
 //이벤트배너
 const eventBanner = () => {};
 
+//공부용↓
+// const sectionPage1 = () => {
+//     const $main = get('#mainVisual');
+//     const $con1 = get('.main .con1');
+//     const $con2 = get('.main .con2');
+//     const $con3 = get('.main .con3');
+//     const $con4 = get('.main .con4');
+//     const $menulis = getAll('.menu li');
+//     let ty = 0;
+//     // console.log($con1.offsetTop);
+//     $menulis[0].addEventListener('click', (e) => {
+//         ty = $main.offsetTop;
+//         window.scrollTo({ top: ty, behavior: 'smooth' });
+//     });
+//     $menulis[1].addEventListener('click', (e) => {
+//         ty = $con1.offsetTop;
+//         window.scrollTo({ top: ty, behavior: 'smooth' });
+//     });
+//     $menulis[2].addEventListener('click', (e) => {
+//         ty = $con2.offsetTop;
+//         window.scrollTo({ top: ty, behavior: 'smooth' });
+//     });
+//     $menulis[3].addEventListener('click', (e) => {
+//         ty = $con3.offsetTop;
+//         window.scrollTo({ top: ty, behavior: 'smooth' });
+//     });
+//     $menulis[4].addEventListener('click', (e) => {
+//         ty = $con4.offsetTop;
+//         window.scrollTo({ top: ty, behavior: 'smooth' });
+//     });
+// };
+
+//참고용
+const sectionPage = () => {
+    const $main = get('#mainVisual');
+    const $cons = getAll('.main .con');
+    console.log($cons[0].offsetTop);
+
+    const posY = [];
+    posY.push($main.offsetTop);
+    $cons.forEach((item) => {
+        posY.push(item.offsetTop);
+    });
+    // console.log(posY);
+
+    const $menulis = getAll('.menu li');
+    $menulis.forEach((itemLi, idx) => {
+        itemLi.addEventListener('click', (e) => {
+            window.scrollTo({ top: posY[idx], behavior: 'smooth' });
+            //모두떼기 -> forEach
+            $menulis.forEach((item) => item.classList.remove('on'));
+            itemLi.classList.add('on');
+        });
+    });
+};
+
+const menuBar = () => {
+    const $menu = get('.menu');
+    const $main = get('#mainVisual');
+    const $cons = getAll('.main .con');
+    const posY = [];
+    posY.push($main.offsetTop);
+    $cons.forEach((item) => {
+        posY.push(item.offsetTop);
+    });
+    let ty = 0;
+    const $menulis = getAll('.menu li');
+    $menulis.forEach((itemLi, idx) => {
+        itemLi.addEventListener('click', (e) => {
+            window.scrollTo({ top: posY[idx], behavior: 'smooth' });
+            //모두떼기 -> forEach
+            // $menulis.forEach((item) => item.classList.remove('on'));
+            // itemLi.classList.add('on');
+            // 튕기는거 지우기
+        });
+    });
+    window.addEventListener('scroll', (e) => {
+        ty = window.scrollY;
+        for (let i = 0; i < $menulis.length; i++) {
+            if (ty >= posY[i]) {
+                $menulis.forEach((item) => item.classList.remove('on'));
+                $menulis[i].classList.add('on');
+            }
+        }
+        // if (ty >= posY[1]) {
+        //     $menulis.forEach((item) => item.classList.remove('on'));
+        //     $menulis[1].classList.add('on');
+        // }
+        // if (ty >= posY[2]) {
+        //     $menulis.forEach((item) => item.classList.remove('on'));
+        //     $menulis[2].classList.add('on');
+        // }
+        // if (ty >= posY[3]) {
+        //     $menulis.forEach((item) => item.classList.remove('on'));
+        //     $menulis[3].classList.add('on');
+        // }
+        // if (ty >= posY[4]) {
+        //     $menulis.forEach((item) => item.classList.remove('on'));
+        //     $menulis[4].classList.add('on');
+        // } else {
+        //     $menu.classList.remove('on');
+        // }
+
+        // top부분
+        if (ty > 400) {
+            $menu.classList.add('on');
+        } else {
+            $menu.classList.remove('on');
+        }
+    });
+};
+
 //합치기
 const mainInit = () => {
     mainBanner();
     eventBanner();
+    menuBar();
+    // sectionPage();
 };
 
 (() => {
